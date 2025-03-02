45cbd7cbe6dad8299fe86e77918402ad450222a8
adnan-joy <adnanjoy10@gmail.com>
2025-03-02 19:36:39 +0600

    dev board project added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports={
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3752fd',
+                text:'#00303c',
+                secondary: '#F4F7FF',
+            },
+            fontFamily: {
+                    poppins: ['Poppins', 'sans-serif'],
+            },
+        }
+    }
+}
